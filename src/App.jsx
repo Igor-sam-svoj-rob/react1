3b08f1ba4e2209501e791b286919b0a095a56a37
Igor-sam-svoj-rob <Ijevremovic@yahoo.com>
2023-06-21 10:22:01 +0200

    Verzija1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Components/Header";
+import Kartica from "./Components/Kartica";
+
+function App() {
+  return (
+    <>
+      <Header text="Novi Logo" />
+      <Kartica />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header({ text }) {
+  return (
+    <header className="container">
+      <p>{text}</p>
+    </header>
+  );
+}
+
+export default Header;
